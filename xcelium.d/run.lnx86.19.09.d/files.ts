243e652c0286a367b9735b7740e033665f788227
sikorah <sikora.hubert@outlook.com>
2024-08-28 19:15:40 +0200

    test gitignore na czystym repo - bez zmian kodu

--- xcelium.d/run.lnx86.19.09.d/files.ts
+++
@@ -1,3 +0,0 @@
-1724668323 /home/student/zschab/Desktop/Projekt_UEC2/rtl/vga_pkg.sv
-1724668323 /home/student/zschab/Desktop/Projekt_UEC2/rtl/vga_if.sv
-1724853708 /home/student/zschab/Desktop/Projekt_UEC2/rtl/draw_buttons.sv
